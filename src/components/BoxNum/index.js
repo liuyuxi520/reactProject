19cd28e649b366926bdec9ba53f069e0ac5db9cb
liufeifeng <liufeifeng@jtoushou.com>
2018-03-02 17:58:15 +0800

    add new component BoxNum

---
+++ src/components/BoxNum/index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by liufeifeng on 3/2/18.
+ */
+import React, { Component } from 'react';
+import './Box.css';
+class Square extends Component {
+    render(){
+        return (
+            <button className="square">{this.props.value}</button>
+        )
+    }
+}
+class Status extends Component {
+    render(){
+        return(
+            <div className="status">{this.props.status}</div>
+        )
+    }
+}
+class Board extends Component {
+    squareRender(i){
+        return(
+            <Square value = {i} />
+        )
+    }
+    render(){
+        return(
+            <div className="board">
+                <Status status={this.props.status} />
+                <div>
+                    <div className="row-square">
+                        {this.squareRender(1)}
+                        {this.squareRender(2)}
+                        {this.squareRender(3)}
+                    </div>
+                    <div className="row-square">
+                        {this.squareRender(4)}
+                        {this.squareRender(5)}
+                        {this.squareRender(6)}
+                    </div>
+                    <div className="row-square">
+                        {this.squareRender(7)}
+                        {this.squareRender(8)}
+                        {this.squareRender(9)}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Board;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
+import Board from './components/BoxNum/index';
 import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<Header name='lyx'/>, document.getElementById('reactNav'));
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Footer name='react' info='lyxReact'/>, document.getElementById('reactFooter'));
+ReactDOM.render(<Board status='请玩九宫格'/>, document.getElementById('reactBox'));
 
 registerServiceWorker();
